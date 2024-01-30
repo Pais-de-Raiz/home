0d1e08d0125fdb199907aa144807831ba3f03d3f
Ivan Roa <naviroa92@Ivans-MacBook-Pro.local>
2024-01-29 19:36:10 -0500

    fist section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Cargar el contenido del footer.html en la sección con id "footer"
-    var footerContainer = document.getElementById('navbar');
+    var navbarContainer = document.getElementById('navbar');
     var xhr = new XMLHttpRequest();
     
     xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
-                footerContainer.innerHTML = xhr.responseText;
+                navbarContainer.innerHTML = xhr.responseText;
             } else {
                 console.error('Error al cargar el navbar: ' + xhr.status);
             }
@@ -33,3 +33,43 @@ document.addEventListener('DOMContentLoaded', function () {
     xhr.open('GET', 'assets/partials/footer.html', true);
     xhr.send();
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Cargar el contenido del footer.html en la sección con id "footer"
+    var propContainer = document.getElementById('prop');
+    var xhr = new XMLHttpRequest();
+    
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                propContainer.innerHTML = xhr.responseText;
+            } else {
+                console.error('Error al cargar el prop-value ' + xhr.status);
+            }
+        }
+    };
+    xhr.open('GET', 'assets/partials/prop.html', true);
+    xhr.send();
+});
+
+// Espera a que el DOM esté cargado
+document.addEventListener("DOMContentLoaded", function () {
+        // Obtén el contenedor de animación dentro de la sección
+        var animationContainer = document.getElementById("animation-container");
+
+        // Verifica si el contenedor existe antes de inicializar la animación
+        if (animationContainer) {
+            // Configura la animación
+            var animation = bodymovin.loadAnimation({
+                container: animationContainer,
+                renderer: 'svg',
+                loop: true,
+                autoplay: true,
+                path: 'img/animation.json' // Reemplaza con la ruta correcta de tu archivo JSON
+            });
+        } else {
+            console.error("No se encontró el contenedor de animación en la sección.");
+        }
+});
+
+
