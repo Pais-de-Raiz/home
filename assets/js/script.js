dce72e6e337b905c1dbdafa2c4e9852b3c6a262f
Ivan Roa <naviroa92@Ivans-MacBook-Pro.local>
2024-02-22 13:20:03 -0500

    fundaciones aliadas

--- assets/js/script.js
+++ assets/js/script.js
@@ -212,3 +212,61 @@ fetch('servicios.json')
         });
     })
     .catch(error => console.error('Error al cargar el archivo JSON:', error));
+
+
+
+
+    var fundacionesContainer = document.getElementById('fundaciones-container');
+
+    fetch('fundaciones-cards.json')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(item => {
+                var cardColumn = document.createElement('div');
+                cardColumn.className = 'col-12 col-md-6 col-lg-4 col-xl-3';
+    
+                var card = document.createElement('div');
+                card.className = 'card';
+    
+                var cardImg = document.createElement('img');
+                cardImg.className = 'card-img-top';
+                cardImg.src = item.card.imgSrc;
+                cardImg.alt = item.card.imgAlt;
+                //estilo
+                card.style.width = '18rem';
+                card.style.margin = '10px auto';
+    
+                var cardBody = document.createElement('div');
+                cardBody.className = 'card-body';
+    
+                var cardTitle = document.createElement('h5');
+                cardTitle.className = 'card-title';
+                cardTitle.innerText = item.card.title;
+
+                var cardLocation = document.createElement('h6');
+                cardLocation.className = 'card-location';
+                cardLocation.innerText = item.card.location;
+    
+                var cardText = document.createElement('p');
+                cardText.className = 'card-text';
+                cardText.innerText = item.card.text;
+    
+                var btn = document.createElement('a');
+                btn.className = 'btn btn-primary';
+                btn.href = item.card.btnLink;
+                btn.innerText = item.card.btnText;
+    
+                cardBody.appendChild(cardTitle);
+                cardBody.appendChild(cardLocation);
+                cardBody.appendChild(cardText);
+                cardBody.appendChild(btn);
+    
+                card.appendChild(cardImg);
+                card.appendChild(cardBody);
+    
+                cardColumn.appendChild(card);
+    
+                fundacionesContainer.appendChild(cardColumn);
+            });
+        })
+        .catch(error => console.error('Error al cargar el archivo JSON:', error));
