a0a366157daf6e51457f4d9e084e839538752d71
Ivan Roa <naviroa92@Ivans-MacBook-Pro.local>
2024-03-09 12:12:50 -0500

    update project

--- assets/js/script.js
+++ assets/js/script.js
@@ -538,4 +538,4 @@ function cargarTarjetasFiltradas2(jsonFile, fundacion, contenedorId) {
         .catch(error => console.error('Error al cargar el archivo JSON:', error));
 }
 
-cargarTarjetasFiltradas2('servicios2-cards.json', 'Biblioseo', 'servicios-detalle');
+cargarTarjetasFiltradas2('https://pais-de-raiz.github.io/home/servicios2-cards.json', 'Biblioseo', 'servicios-detalle');
