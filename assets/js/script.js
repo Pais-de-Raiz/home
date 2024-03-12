86979429582612050d8768ec3f5112dab8e5b451
Ivan Roa <naviroa92@Ivans-MacBook-Pro.local>
2024-03-11 20:18:59 -0500

    prueba 25

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     };
-    xhr.open('GET', "../assets/partials/head.html", true);
+    xhr.open('GET', "../../assets/partials/head.html", true);
     xhr.send();
 });
 
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     };
-    xhr.open('GET', 'assets/partials/navbar.html', true);
+    xhr.open('GET', '../../assets/partials/navbar.html', true);
     xhr.send();
 });
 
