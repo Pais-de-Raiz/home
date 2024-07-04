2eeb6f606947e6d7e7fa7cf1dc7f7ab7f808c018
Ivan Roa <naviroa92@Ivans-MacBook-Pro.local>
2024-07-03 20:50:13 -0500

    update test v4

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import json
 import gspread
 from google.oauth2.service_account import Credentials
 from jinja2 import Environment, FileSystemLoader
-import os  # Importar el módulo os para trabajar con archivos y directorios
+import os
 
 # Define los ámbitos necesarios para acceder a Google Sheets y Google Drive
 scopes = [
@@ -23,11 +23,7 @@ sh = gc.open_by_key(id_documento)
 
 # Tu lógica aquí...
 hoja_de_calculo = gc.open_by_key(id_documento)
-hoja = hoja_de_calculo.sheet1  # Puedes cambiar el nombre de la hoja si es necesario
-
-# Solicitud de acceso a documento
-hoja_de_calculo = gc.open_by_key(id_documento)
-nombre_de_la_pestaña = "servicios-detalle"  # Cambia esto si tu pestaña tiene otro nombre
+nombre_de_la_pestaña = "servicios-detalle"
 pestaña = hoja_de_calculo.worksheet(nombre_de_la_pestaña)
 
 # Obtener todos los datos de la hoja
@@ -46,7 +42,7 @@ for row in data:
     codigo = row['codigo']
     nombre_servicio = row['experiencia']
     capacidad = row['capacidad']
-    cantidad = row['cantidad']  # Se lee el campo cantidad, pero no se usa en el HTML
+    cantidad = row['cantidad']
     unidad = row['unidad']
     modalidad = row['modalidad']
     url_reserva = row['URL de Reserva']
@@ -55,16 +51,16 @@ for row in data:
 
     # Convertir los saltos de línea en <br>
     descripcion_1 = descripcion_1.replace('\n', '<br>\n')
-    
+
     # Crear el id dinámico para el div
     id_servicio = f"servicio-{codigo}"
-    
+
     # Usar 'nombre_servicio' como el título largo
     titulo_largo = nombre_servicio
-    
+
     # Renderizar la plantilla con los datos de la fila
     html_content = template.render(
-        id_servicio=id_servicio,  # Agregamos este dato a la plantilla
+        id_servicio=id_servicio,
         capacidad=capacidad,
         unidad=unidad,
         cantidad=cantidad,
@@ -73,11 +69,21 @@ for row in data:
         categoria=categoria,
         nombre_servicio=nombre_servicio,
         titulo_largo=titulo_largo,
-        descripcion_1=descripcion_1  # Pasamos el texto con <br> a la plantilla
+        descripcion_1=descripcion_1
     )
-    
+
     # Guardar el contenido HTML en un archivo dentro de la carpeta experiencia-detalle
-    with open(f"experiencia-detalle/{codigo}.html", "w") as f:
-        f.write(html_content)
+    file_path = f"experiencia-detalle/{codigo}.html"
+    
+    # Comparar contenido si el archivo existe
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as f:
+            existing_content = f.read()
+        if existing_content != html_content:
+            with open(file_path, 'w') as f:
+                f.write(html_content)
+    else:
+        with open(file_path, 'w') as f:
+            f.write(html_content)
 
 print("Documentos HTML generados con éxito.")
