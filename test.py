9e669b127b7f93acbfcd8392b2fa76a71b427ee1
Ivan Roa <naviroa92@Ivans-MacBook-Pro.local>
2024-07-03 20:34:11 -0500

    update test v2

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import json
 import gspread
 from google.oauth2.service_account import Credentials
 from jinja2 import Environment, FileSystemLoader
-import os  # Importar el módulo os para trabajar con archivos y directorios
+import os
 
 # Define los ámbitos necesarios para acceder a Google Sheets y Google Drive
 scopes = [
@@ -12,7 +12,7 @@ scopes = [
 
 # Lee las credenciales desde el archivo client_secrets.json
 with open('client_secrets.json') as f:
-    creds_dict = json.load(f)  # Asegúrate de que el archivo tiene un JSON válido
+    creds_dict = json.load(f)
 
 creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
 gc = gspread.authorize(creds)
@@ -22,8 +22,9 @@ id_documento = '1LlilYZIDVp4al8WGNr1sdZg-OuQW6jSK5zii6zHAVwU'
 sh = gc.open_by_key(id_documento)
 
 # Solicitud de acceso a documento
-nombre_de_la_pestaña = "servicios-detalle"  # Cambia esto si tu pestaña tiene otro nombre
-pestaña = sh.worksheet(nombre_de_la_pestaña)
+hoja_de_calculo = gc.open_by_key(id_documento)
+nombre_de_la_pestaña = "servicios-detalle"
+pestaña = hoja_de_calculo.worksheet(nombre_de_la_pestaña)
 
 # Obtener todos los datos de la hoja
 data = pestaña.get_all_records()
@@ -41,7 +42,7 @@ for row in data:
     codigo = row['codigo']
     nombre_servicio = row['experiencia']
     capacidad = row['capacidad']
-    cantidad = row['cantidad']  # Se lee el campo cantidad, pero no se usa en el HTML
+    cantidad = row['cantidad']
     unidad = row['unidad']
     modalidad = row['modalidad']
     url_reserva = row['URL de Reserva']
@@ -59,7 +60,7 @@ for row in data:
     
     # Renderizar la plantilla con los datos de la fila
     html_content = template.render(
-        id_servicio=id_servicio,  # Agregamos este dato a la plantilla
+        id_servicio=id_servicio,
         capacidad=capacidad,
         unidad=unidad,
         cantidad=cantidad,
@@ -68,12 +69,11 @@ for row in data:
         categoria=categoria,
         nombre_servicio=nombre_servicio,
         titulo_largo=titulo_largo,
-        descripcion_1=descripcion_1  # Pasamos el texto con <br> a la plantilla
+        descripcion_1=descripcion_1
     )
     
     # Guardar el contenido HTML en un archivo dentro de la carpeta experiencia-detalle
-    file_path = f"experiencia-detalle/{codigo}.html"
-    with open(file_path, "w") as f:
+    with open(f"experiencia-detalle/{codigo}.html", "w") as f:
         f.write(html_content)
 
 print("Documentos HTML generados con éxito.")
